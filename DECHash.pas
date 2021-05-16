6b10020be874fad722263b9304ac701b2320ede2
decfpc <33814197+decfpc@users.noreply.github.com>
2021-05-16 03:02:14 +0200

    Remove duplicate include from DECHash

--- DECHash.pas
+++ DECHash.pas
@@ -386,7 +386,6 @@ implementation
 uses DECData;
 
 {$I DECHash.inc}
-{$I Ver.inc}
 
 {                                        assembler                             pascal
 THash_SHA512        :       85.1 cycles/byte      17.62 Mb/sec      220.9 cycles/byte       6.79 Mb/sec  159%

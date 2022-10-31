c98a731307b8cd4108e581a470da9111feb5b1a6
alexrayne <none@none>
2022-10-31 17:16:24 +0300

    +package: lazarus

---
+++ Packages/FPC/dec.pas
@@ -0,0 +1,22 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit DEC;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  CPU, DECCipher, DECCRC, DECData, DECFmt, DECHash, DECRandom, DECUtil, 
+  LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('DEC', @Register);
+end.
